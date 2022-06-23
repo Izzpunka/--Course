8d8d54ea6b12dc84360cc811455fa33c738f8878
Elena <harahonova@mail.ru>
2022-06-23 11:38:15 +0300

    Task 1_3

---
+++ 1st_lesson/1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int day = int.Parcel(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (day == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (day == 4)
+{
+    Console.WriteLine("Четверг");
+}
