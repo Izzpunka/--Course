efab3138686f2b374265d09dd38553dd12a89391
Elena <harahonova@mail.ru>
2022-06-23 11:14:09 +0300

    Task 1_1

--- 1st_lesson/1_1/Program.cs
+++ 1st_lesson/1_1/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿//String num_str = Console.ReadLine();
+//int num = int.Parse(num_str);
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 * num1 == num2)
+{
+    Console.WriteLine("да");
+}
+else
+
+{
+    Console.WriteLine("нет");
+}
+
+
+
