cffe872acb623327b1e9fa11231fa3ee932f51a0
Elena <harahonova@mail.ru>
2022-06-30 00:13:04 +0300

    HW2

--- 1st_lesson/2nd_lesson/2_1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿int TakeNum(int num)
-{
-    return num % 10;
-}
-
-Console.WriteLine(TakeNum(678));
-Console.WriteLine(TakeNum(678565565));
---
+++ 1st_lesson/HomeWork/HW2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+string s_n_1 = Console.ReadLine();
+string s_n_2 = Console.ReadLine();
+
+int n1 = int.Parse(s_n_1);
+int n2 = int.Parse(s_n_2);
+
+if (n1 > n2)
+{
+Console.WriteLine($"The number {n1} is bigger than {n2}");
+}
+else if (n1 == n2)
+{
+Console.WriteLine($"The number {n1} is equal t0 {n2}");
+}
+else
+{
+    Console.WriteLine($"The number {n2} is bigger than {n1}");
+}
