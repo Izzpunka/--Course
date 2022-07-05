4eabf105590d3a2ce45c854560b42fd61885ad90
Elena <harahonova@mail.ru>
2022-07-05 22:07:40 +0300

    HW19

---
+++ 3rd_lesson/HomeWork_3rd_lesson/HW_task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+void Palindrome(int n)
+{
+    int i = 0;
+    string num = n.ToString();
+    int size = num.Length - 1;
+    string palindrome = "yes";
+    
+    while (i < size / 2)
+    {
+        if (num[i] != num[size - i]) 
+        {
+            palindrome = "no";
+            break;
+        }        
+        i += 1;
+    }    
+    Console.WriteLine($"{n} -> {palindrome}");
+}
+
+Palindrome(54345);
