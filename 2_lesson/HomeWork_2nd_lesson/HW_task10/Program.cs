946174b849d44bfab68d818860f25f1901f40c24
Elena <harahonova@mail.ru>
2022-07-05 13:03:30 +0300

    HW10

---
+++ 2_lesson/HomeWork_2nd_lesson/HW_task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+
+Console.WriteLine("Введите трехзначное число");
+string userData = Console.ReadLine();
+int userNumber = Convert.ToInt32(userData);
+int secondNumber(int userNumber)
+{
+return userNumber/10 % 10;
+}
+Console.WriteLine("Вторая цифра:" + secondNumber(userNumber));
