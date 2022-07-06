7e4ff76eafc7801b0bf06c0b678ae9ac83ec58e2
Elena <harahonova@mail.ru>
2022-07-06 12:47:30 +0300

    HW23

--- 3rd_lesson/HomeWork_3rd_lesson/HW_task23/Program.cs
+++ 3rd_lesson/HomeWork_3rd_lesson/HW_task23/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая принимает на вход число (N)
+//    и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Table(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    Console.Write($"{n} -> ");
+
+    while (index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 3);
+        Console.Write($"{nums[index]}, ");
+        index += 1;
+    }    
+    Console.WriteLine();
+}
+
+Table(3);
+Table(5);
+
