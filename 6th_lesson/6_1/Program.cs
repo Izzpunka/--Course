8a158a8c503beb1d9c9f51304b8514e0bff283e8
Elena <harahonova@mail.ru>
2022-07-11 11:09:42 +0300

    6_2

--- 6th_lesson/6_1/Program.cs
+++ 6th_lesson/6_1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+void Triangle (int num1, int num2, int num3)
+{
+    if(num1+num2>num3 & num1+num3>num2 & num2+num3>num1); 
+
+Console.WriteLine("Triangle exists");
+else Console.WriteLine("Triangle doesn`t exist");
+}
+Triangle(5,6,3);
+
+
+
+Сравнить
+// // Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами
+// // такой длины.
+
+// void Triangle (int numA,int numB, int numC)
+// {
+//     if(numA + numB > numC && numA + numC > numB && numB + numC > numA)
+//     Console.WriteLine("Треугольник существует!");
+//     else Console.WriteLine("Нет!");
+// }
+
+// Triangle(1, 2, 3);
+
+
+
--- 6th_lesson/6_2/Program.cs
+++ 6th_lesson/6_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+void Binary (int number)
+{
+    string Dec = "";
+    while (number>0)
+    {
+        Dec = number % 2 + Dec;
+        number /=2;
+    }
+Console.WriteLine(Dec);
+}
+Binary(2);
