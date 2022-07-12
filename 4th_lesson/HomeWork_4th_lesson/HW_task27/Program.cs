b21d586582a7f3637ae141e1fde7317d88be1edc
Elena <harahonova@mail.ru>
2022-07-12 23:13:51 +0300

    hw_task27

---
+++ 4th_lesson/HomeWork_4th_lesson/HW_task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int num)
+{
+    int sum = 0;
+
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+Console.WriteLine($"Result = {SumNum(num)}");
