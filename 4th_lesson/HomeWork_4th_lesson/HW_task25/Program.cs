0e0e2b11d7a0025b2936a347f2f946026b6413b9
Elena <harahonova@mail.ru>
2022-07-12 22:47:44 +0300

    hw_task25

---
+++ 4th_lesson/HomeWork_4th_lesson/HW_task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Enter number А");
+string inputA = Console.ReadLine();
+int a = Convert.ToInt32(inputA);
+Console.WriteLine("Enter number B");
+string inputB = Console.ReadLine();
+int b = Convert.ToInt32(inputB);
+
+int naturStepen(int a, int b)
+{
+    int result = a;
+    for (int i = 2; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+Console.WriteLine(naturStepen(a,b));
