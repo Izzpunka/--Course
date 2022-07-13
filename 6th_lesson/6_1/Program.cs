668e5fcab95630eed46cdc914dee685e1ae2f3f3
Elena <harahonova@mail.ru>
2022-07-13 12:00:47 +0300

    6_1

--- 6th_lesson/6_1/Program.cs
+++ 6th_lesson/6_1/Program.cs
@@ -3,27 +3,15 @@
 
 void Triangle (int num1, int num2, int num3)
 {
-    if(num1+num2>num3 & num1+num3>num2 & num2+num3>num1); 
+    if(num1+num2>num3 && num1+num3>num2 && num2+num3>num1) 
 
-Console.WriteLine("Triangle exists");
-else Console.WriteLine("Triangle doesn`t exist");
+Console.WriteLine($"Triangle exists");
+
+else
+{
+    Console.WriteLine("Triangle doesn`t exist"); 
 }
+}
+
 Triangle(5,6,3);
-
-
-
-Сравнить
-// // Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами
-// // такой длины.
-
-// void Triangle (int numA,int numB, int numC)
-// {
-//     if(numA + numB > numC && numA + numC > numB && numB + numC > numA)
-//     Console.WriteLine("Треугольник существует!");
-//     else Console.WriteLine("Нет!");
-// }
-
-// Triangle(1, 2, 3);
-
-
 
