84f2fc981548e39111208a5dbf0e19f313fbe09a
Elena <harahonova@mail.ru>
2022-07-21 11:00:17 +0300

    9_3

--- 9th_lesson/9_3/Program.cs
+++ 9th_lesson/9_3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int NumbersSum(int number)
+{
+
+    if (number == 0) return 0;
+    return NumbersSum(number / 10) + number % 10;
+}
+
+Console.WriteLine(NumbersSum(453));
